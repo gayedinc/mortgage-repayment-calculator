fe0b9df50c67f50236d9042d067aacb592901807
[Gaye Dinç] <gayedinc190@gmail.com>
2024-12-02 16:52:27 +0300

    validasyon güncellemeleri yapıldı

--- assets/js/mortgage.js
+++ assets/js/mortgage.js
@@ -1,100 +1,132 @@
-amountInput.addEventListener("change", handleAmount);
-termInput.addEventListener("change", handleTerm);
-rateInput.addEventListener("change", handleRate);
-
 calcBtn.addEventListener("click", handleCalc);
 clearBtn.addEventListener("click", handleClear);
 
-function handleAmount() {
-  if (amountInput.value.trim() !== '') {
-    amountInput.innerHTML += amountInput.value;
-  }
-}
-
-function handleTerm() {
-  if (termInput.value.trim() !== '') {
-    termInput.innerHTML += termInput.value;
-  }
-}
-
-function handleRate() {
-  if (rateInput.value.trim() !== '') {
-    rateInput.innerHTML += rateInput.value;
-  }
-}
-
+// başlangıçta tüm input alanlarına c-blue sınıfı ekliyorum
 document.querySelector('.input-amount').classList.add('c-blue');
 document.querySelector('.input-rate').classList.add('c-blue');
 document.querySelector('.input-term ').classList.add('c-blue');
-
 
 let totalRate = 0; //toplam faiz
 let totalRepay = 0; //toplam geri ödeme
 let monthlyPay = 0; // aylık ödeme
 
 function handleCalc() {
-  if (amountInput.value === "" || termInput.value === "" || rateInput.value === "") {
-    // Hata mesajlarını ve inputları kırmızıya dönüştür
+  let isValid = true; // başlangıçta tüm inputlar geçerli kabul edilecek
+
+  // amount input validasyonu
+  if (amountInput.value === "" || amountInput.value === "0") {
     document.querySelector('.amount-icon').classList.remove('c-blue');
     document.querySelector('.amount .error-msg').style.display = 'inline';
-
-    document.querySelector('.term-icon').classList.remove('c-blue');
-    document.querySelector('.term .error-msg').style.display = 'inline';
-
-    document.querySelector('.rate-icon').classList.remove('c-blue');
-    document.querySelector('.rate .error-msg').style.display = 'inline';
-
+    document.querySelector('.amount .error-msg').textContent = amountInput.value === "0" ? "Can't be zero" : "This field is required";
     document.querySelector('.input-amount').classList.remove('c-blue');
     document.querySelector('.input-amount').classList.add('c-red');
+    isValid = false;
+  } else {
+    // hata mesajını gizle ve sadece c-yellow sınıfını ekle
+    document.querySelector('.amount-icon').classList.remove('c-blue');
+    document.querySelector('.amount .error-msg').style.display = 'none';
+    document.querySelector('.input-amount').classList.remove('c-red');
+    document.querySelector('.input-amount').classList.add('c-yellow');
+  }
 
+  // term input validasyonu
+  if (termInput.value === "" || termInput.value === "0") {
+    document.querySelector('.term-icon').classList.remove('c-blue');
+    document.querySelector('.term .error-msg').style.display = 'inline';
+    document.querySelector('.term .error-msg').textContent = termInput.value === "0" ? "Can't be zero" : "This field is required";
     document.querySelector('.input-term').classList.remove('c-blue');
     document.querySelector('.input-term').classList.add('c-red');
+    isValid = false;
+  } else {
+    // hata mesajını gizle ve c-yellow sınıfını ekle
+    document.querySelector('.term-icon').classList.remove('c-blue');
+    document.querySelector('.term .error-msg').style.display = 'none';
+    document.querySelector('.input-term').classList.remove('c-red');
+    document.querySelector('.input-term').classList.add('c-yellow');
+  }
 
+  // rate input validasyonu
+  if (rateInput.value === "" || rateInput.value === "0") {
+    document.querySelector('.rate-icon').classList.remove('c-blue');
+    document.querySelector('.rate .error-msg').style.display = 'inline';
+    document.querySelector('.rate .error-msg').textContent = rateInput.value === "0" ? "Can't be zero" : "This field is required";
     document.querySelector('.input-rate').classList.remove('c-blue');
     document.querySelector('.input-rate').classList.add('c-red');
-
-    document.querySelector('.type .error-msg').style.display = 'inline';
-
-    // Results kısmını gizle
-    document.querySelector('.results').classList.add('d-none');
-
-    // ShownTxt'yi göster
-    document.querySelector('.shownTxt').classList.remove('d-none');
-    document.querySelector('.shownTxt').classList.add('d-blok');
+    isValid = false;
   } else {
-    // Hesaplama işlemleri
+    // hata mesajını gizle ve sadece c-yellow sınıfını ekle
+    document.querySelector('.rate-icon').classList.remove('c-blue');
+    document.querySelector('.rate .error-msg').style.display = 'none';
+    document.querySelector('.input-rate').classList.remove('c-red');
+    document.querySelector('.input-rate').classList.add('c-yellow');
+  }
+
+  // mortgage type kontrolü
+  const mortgageTypeChecked = document.querySelector('input[name="option"]:checked');
+  if (!mortgageTypeChecked) {
+    document.querySelector('.type .error-msg').style.display = 'inline';
+    isValid = false;
+  } else {
+    document.querySelector('.type .error-msg').style.display = 'none';
+  }
+
+  // eğer her input değeri geçerliyse hesaplama yapılacak
+  if (isValid) {
+    // hesaplama işlemleri
     monthlyRate = Number(rateInput.value) / 100 / 12;
-    totalMonthlyPayNumber = Number((termInput.value) * 12);
-    monthlyPay = (Number(amountInput.value) * monthlyRate) / (1 - Math.pow((1 + monthlyRate), - totalMonthlyPayNumber));
+    totalMonthlyPayNumber = Number(termInput.value) * 12;
+    monthlyPay = (Number(amountInput.value) * monthlyRate) / (1 - Math.pow((1 + monthlyRate), -totalMonthlyPayNumber));
     totalRepay = Number(monthlyPay * totalMonthlyPayNumber);
     monthlyPrice.innerText = monthlyPay.toFixed(2);
     totalPrice.innerText = totalRepay.toFixed(2);
 
-    // Hata mesajlarını ve inputları sarıya dönüştür
-    document.querySelector('.input-amount').classList.remove('c-blue');
-    document.querySelector('.input-term').classList.remove('c-blue');
-    document.querySelector('.input-rate').classList.remove('c-blue');
-
+    // hata mesajlarını gizle ve inputları sarıya döndür
     document.querySelector('.amount .error-msg').style.display = 'none';
     document.querySelector('.term .error-msg').style.display = 'none';
     document.querySelector('.rate .error-msg').style.display = 'none';
     document.querySelector('.type .error-msg').style.display = 'none';
 
+    // `c-blue` sınıfını kaldır ve `c-yellow` ekle
+    document.querySelector('.input-amount').classList.remove('c-blue');
+    document.querySelector('.input-term').classList.remove('c-blue');
+    document.querySelector('.input-rate').classList.remove('c-blue');
+
     document.querySelector('.input-amount').classList.add('c-yellow');
     document.querySelector('.input-term').classList.add('c-yellow');
     document.querySelector('.input-rate').classList.add('c-yellow');
 
-    // Results'ı göster
-    document.querySelector('.shownTxt').classList.add('d-none'); // shownTxt'yi gizle
-    document.querySelector('.results').classList.remove('d-none'); // results'ı göster
-    document.querySelector('.results').classList.add('d-blok'); // results'ı bloğa al
+    // shownTxt'yi gizle ve sonuçları göster
+    document.querySelector('.shownTxt').classList.add('d-none');
+    document.querySelector('.results').classList.remove('d-none');
+    document.querySelector('.results').classList.add('d-blok');
+  } else {
+    // geçersizse sonuçları gizle
+    document.querySelector('.results').classList.add('d-none');
   }
 }
 
 function handleClear() {
+  // tüm input değerlerini boşalt
   amountInput.value = "";
   termInput.value = "";
   rateInput.value = "";
   monthlyPrice.innerText = "";
   totalPrice.innerText = "";
+
+  // hata mesajlarını gizlemek için
+  document.querySelector('.amount .error-msg').style.display = 'none';
+  document.querySelector('.term .error-msg').style.display = 'none';
+  document.querySelector('.rate .error-msg').style.display = 'none';
+  document.querySelector('.type .error-msg').style.display = 'none';
+
+  // temizleme sonrası inputlara c-blue sınıfı ekle
+  document.querySelector('.input-amount').classList.add('c-blue');
+  document.querySelector('.input-rate').classList.add('c-blue');
+  document.querySelector('.input-term').classList.add('c-blue');
+
+  // radio butonlarının seçimini kaldırmak için
+  const radioButtons = document.querySelectorAll('input[name="option"]');
+  radioButtons.forEach(button => {
+    button.checked = false;
+  });
 }
